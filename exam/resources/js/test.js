ff57094c55ee2dec4f87e8b4b63a3ec86eff4442
cheongha12 <cheong.9396@gmail.com>
2023-06-27 15:07:34 +0900

    .....

---
+++ exam/resources/js/test.js
@@ -0,0 +1,13 @@
+// <script> 태그는 작성하지 않음.
+// 팝업창 종류 
+//1. alret -확인버튼만 있는 창
+alert("이건 alert");
+//2.confirm- 확인/취소(true/false값이 return됨) 버튼 창
+var confirm= confirm("컨펌창");
+alert(confirValue);
+
+//3.promt-입력-확인/취소(입력한 값이 return됨) 버튼 창
+var promptValue= prompt("컨펌창");
+alert(promptValue);
+console.log(promptValue);
+document.write(promptValue);
